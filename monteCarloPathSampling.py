550fcfc48951a8c660921b16a42dd514daa0be17
David Hui <dyth2@cam.ac.uk>
2017-06-13 11:54:06 +0100

    added currying for evaluation function

--- monteCarloPathSampling.py
+++ monteCarloPathSampling.py
@@ -2,26 +2,34 @@
 """module for Monte Carlo Path Sampling"""
 import random
 
-def randomStep(dimensionality):
+def randomStep(dimensionality, stepSize):
     'do a valid step in a random walk'
     # create random vector, normalise, then multiply by step size
-    step = [random.uniform(-1.0, 1.0) for _ in dimensionality]
+    step = [random.uniform(-1.0, 1.0) for _ in range(dimensionality)]
     magnitude = sum([i**2.0 for i in step])
     step = [stepSize * i / magnitude for i in step]
     return step
     
 
-def randomWalk(depth, dimensionality):
+def randomWalk(walk, depth, dimensionality, stepSize, valid):
     'do a random walk for depth steps'
-    walk = []
-    for _ in range(depth):
-        walk.append(randomStep(dimensionality))
-        while (not valid(walk)):
-            walk[-1] = randomStep(dimensionality)
-    return walk
+    if (depth == 0):
+        # base case
+        return walk
+    else:
+        # generate next step and add to current position
+        step = randomStep(dimensionality, stepSize)
+        point = [step[i] + walk[-1][i] for i in range(dimensionality)]
+        if (not valid(walk, point)):
+            # if invalid, do the computation again by recursion
+            return randomWalk(walk, depth, dimensionality, stepSize, valid)
+        else:
+            # if valid, then append to history, then recurse
+            walk.append(point)
+            return randomWalk(walk, depth-1, dimensionality, stepSize, valid)
 
 
-def monteCarloPathSampling(number, depth, dimensionality):
+def monteCarloPathSampling(start, number, depth, dimensionality, stepSize, valid):
     'do number of monte carlo random walks at depth'
     for _ in range(number):
-        walk = randomWalk(depth, dimensionality)
+        walk = randomWalk([start], depth, dimensionality, stepSize, valid)
--- particleBox.py
+++ particleBox.py
@@ -3,9 +3,20 @@
 from monteCarloPathSampling import *
 
 stepSize = 5.0
-initialState = (x, y)
+depth = 1
+start = [-50.0, 0.0]
+bounds = ((-100.0, 100.0), (-100.0, 100.0))
+dimensionality = len(bounds)
 
-def valid()
+
+def valid(walk, p):
+    'determine whether a walk is valid'
+    if (p[0] < bounds[0][0]) or (p[1] > bounds[0][1]) or (p[0] < bounds[1][0]) or (p[1] > bounds[1][1]):
+        return False
+    else:
+        return True
 
 
 
+while True:
+     print randomWalk([start], depth, dimensionality, stepSize, valid)
