d612cb71063629a3144884c89b2c919127202ed5
David Hui <dyth2@cam.ac.uk>
2017-06-20 14:31:43 +0100

    example adapted from scipy cookbook

--- stochasticMarkovChainSampling.py
+++ stochasticMarkovChainSampling.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python
-"""stochastic path sampling with markov chain probability output"""
-import random, math
+"""stochastic path sampling with Weiner process
+   Output markov chain probability output"""
+import numpy
+from pylab import plot, show, grid, axis, xlabel, ylabel, title
+from brownian import *
 
 
-def randomStep(dims, stepSize):
-    'do a valid step in a random walk'
-    # create random vector, normalise, then multiply by step size
-    step = [random.uniform(-1.0, 1.0) for _ in range(dims)]
-    magnitude = math.sqrt(sum([i**2.0 for i in step]))
-    return [stepSize * i / magnitude for i in step]
-    
+# The Wiener process parameter.
+delta = 0.25
+# Total time.
+T = 10.0
+# Number of steps.
+N = 500
+# Time step size
+dt = T/N
+# Initial values of x.
+x = numpy.empty((2,N+1))
+x[:, 0] = 0.0
 
-def randomWalk(walk, depth, dims, stepSize, valid):
-    'do a random walk for depth steps'
-    if (depth == 0):
-        # base case
-        return walk
-    else:
-        # generate next step and add to current position
-        step = randomStep(dims, stepSize)
-        point = [int(step[i] + walk[-1][i]) for i in range(dims)]
-        if (not valid(walk, point)):
-            # if invalid, do the computation again by recursion
-            return randomWalk(walk, depth, dims, stepSize, valid)
-        else:
-            # if valid, then append to history, then recurse
-            walk.append(point)
-            return randomWalk(walk, depth-1, dims, stepSize, valid)
+brownian(x[:,0], N, dt, delta, out=x[:,1:])
 
+# Plot the 2D trajectory.
+plot(x[0],x[1])
 
-def monteCarloPathSampling(start, number, depth, dims, stepSize, valid):
-    'do number of monte carlo random walks at depth'
-    walks = []
-    for _ in range(number):
-        walks.append(randomWalk([start], depth, dims, stepSize, valid)[-1])
-    return walks
+# Mark the start and end points.
+plot(x[0,0],x[1,0], 'go')
+plot(x[0,-1], x[1,-1], 'ro')
+
+# More plot decorations.
+title('2D Brownian Motion')
+xlabel('x', fontsize=16)
+ylabel('y', fontsize=16)
+axis('equal')
+grid(True)
+show()
