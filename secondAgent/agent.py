7a09ef6cbcf135b12808bb44ada3df38c2530650
David Hui <dyth2@cam.ac.uk>
2017-11-16 18:41:32 +0000

    debouncing convergence test

--- secondAgent/agent.py
+++ secondAgent/agent.py
@@ -3,26 +3,47 @@
 from particleBox import ParticleBox
 from langevin import *
 from scipy.stats import norm
+import numpy as np
 import math, sys
 
 
-def entropic_forcing(logProbs, numSamples, environment, forces):
+def entropic_forcing(logProbs, environment, forces):
     'calculate the path integral and determine the next best move'
-    path_integral = 0.0
+    pathIntegral = 0.0
     for i, f in enumerate(forces):
         # calculate the partial volume v and then weighted sum to path_integral
         v = np.log(sum([np.exp(lP - logProbs[i]) for lP in logProbs]))
-        path_integral += f * v
-    nom = 2.0 * environment.TC * path_integral * environment.TIMESTEP ** 2
-    denom = environment.TR * numSamples * environment.MASS
+        pathIntegral += np.multiply(f, v)
+    nom = 2.0 * environment.TC * pathIntegral * environment.TIMESTEP ** 2
+    denom = environment.TR * (len(forces) + 1) * environment.MASS
     return nom / denom
 
 
-plot = False
-path, numSamples = [], 250
+def debounce_entropic_forcing(u, environment):
+    'calculate the path integral until it converges'
+    walks, logPr, forces = [], [], []
+    pathIntegral, pI = np.array([None, 2.0]), np.array([1.0, 1.0])
+    while not np.array_equal(pathIntegral, pI):
+        pI = pathIntegral
+        w, lP, initialForce = random_walk(u, environment)
+        walks.append(w)
+        logPr.append(lP)
+        forces.append(initialForce)
+        pathIntegral = entropic_forcing(logPr, environment, forces)
+        print len(forces), pathIntegral
+    return pathIntegral
+
+
+
+plot = True
+path, numSamples = [], 500
 environment = ParticleBox()
 pos = environment.start
 print pos
+"""
+debounce_entropic_forcing(pos, environment)
+
+"""
 while True:
     walks, logProbs, f = monte_carlo_path_sampling(numSamples, pos, environment)
 
@@ -32,10 +53,11 @@ while True:
             plt.plot([wi[0] for wi in w], [wi[1] for wi in w])
         plt.show()
         
-    move = entropic_forcing(logProbs, numSamples, environment, f)
+    move = entropic_forcing(logProbs, environment, f)
     pos += move
     if not environment.valid(path, pos):
         print "Error: Agent in invalid environment state,", pos
         sys.exit()
     print pos
     path.append(pos)
+
--- secondAgent/langevin.py
+++ secondAgent/langevin.py
@@ -8,15 +8,16 @@ import math
 
 def step(u, environment):
     'compute next distance by Forward Euler'
-    f = environment.DISTRIBUTION.rvs(environment.DIMS)
-    pos = u + (f * (environment.TIMESTEP) ** 2.0) / environment.MASS
-    return pos, environment.DISTRIBUTION.logpdf(f), f
+    force = [environment.DISTRIBUTION.rvs() for _ in range(environment.DIMS)]
+    logProb = [environment.DISTRIBUTION.logpdf(f) for f in force]
+    pos = u + (np.array(force) * environment.TIMESTEP ** 2.0) / environment.MASS
+    return pos, logProb, force
 
 
 def random_walk(u, environment):
     'Langevin random walk from force for TAU / TIMESTEP steps'
     positions, count = [u], int(environment.TAU / environment.TIMESTEP)
-    logPr, force = 0.0, []
+    logPr, force = np.zeros(environment.DIMS), []
     while count != 0:
         u, lP, f = step(positions[-1], environment)
         # if valid then redo
@@ -25,14 +26,14 @@ def random_walk(u, environment):
             count -= 1
             logPr += lP
             force.append(f)
-    return positions, logPr, force[1] - force[0]
+    return positions, logPr, np.array(force[1]) - np.array(force[0])
 
 
-def monte_carlo_path_sampling(number, u, environment):
+def monte_carlo_path_sampling(number, pos, environment):
     'do number walks within environment with starting position u and velocity v'
     walks, logPr, forces = [], [], []
     for i in range(number):
-        w, lP, initialForce = random_walk(u, environment)
+        w, lP, initialForce = random_walk(pos, environment)
         walks.append(w)
         logPr.append(lP)
         forces.append(initialForce)
