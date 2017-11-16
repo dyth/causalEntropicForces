9e41bad7b9ddd3320903e1f76882c80550c8371c
David Hui <dyth2@cam.ac.uk>
2017-11-16 02:23:02 +0000

    seems to be matching up with the paper

--- secondAgent/agent.py
+++ secondAgent/agent.py
@@ -3,36 +3,39 @@
 from particleBox import ParticleBox
 from langevin import *
 from scipy.stats import norm
-import math
+import math, sys
 
 
-def entropic_forcing(walks, logProbs, numSamples):
+def entropic_forcing(logProbs, numSamples, environment, forces):
     'calculate the path integral and determine the next best move'
     path_integral = 0.0
-    for i, w in enumerate(walks):
+    for i, f in enumerate(forces):
         # calculate the partial volume v and then weighted sum to path_integral
         v = np.log(sum([np.exp(lP - logProbs[i]) for lP in logProbs]))
-        path_integral += (w[i][1] - w[i][0]) * v
-    return 2 * Tc * path_integral / (Tr * numSamples * 100)
+        path_integral += f * v
+    nom = 2.0 * environment.TC * path_integral * environment.TIMESTEP ** 2
+    denom = environment.TR * numSamples * environment.MASS
+    return nom / denom
 
 
+plot = False
+path, numSamples = [], 250
 environment = ParticleBox()
-monte_carlo_path_sampling(20, environment.start, environment)
-"""
-distribution = norm(mean, stdev)
-config = Configuration(distribution, dims, valid)
-path = []
-numSamples = 100
-depth = tau / timeStep
-pos = start
+pos = environment.start
 print pos
 while True:
-    walks, logProbs = monteCarloGaussianPaths(pos, numSamples, config, depth)
-    move = entropic_forcing(walks, logProbs, numSamples)
+    walks, logProbs, f = monte_carlo_path_sampling(numSamples, pos, environment)
+
+    if plot:
+        plt.figure()
+        for w in walks:
+            plt.plot([wi[0] for wi in w], [wi[1] for wi in w])
+        plt.show()
+        
+    move = entropic_forcing(logProbs, numSamples, environment, f)
     pos += move
-    if not valid(path, pos):
+    if not environment.valid(path, pos):
         print "Error: Agent in invalid environment state,", pos
         sys.exit()
     print pos
     path.append(pos)
-"""
--- secondAgent/langevin.py
+++ secondAgent/langevin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Inspired by https://github.com/eholmgren/langevin"""
 import numpy as np
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import math
 
@@ -8,26 +9,31 @@ import math
 def step(u, environment):
     'compute next distance by Forward Euler'
     f = environment.DISTRIBUTION.rvs(environment.DIMS)
-    a = f / environment.MASS
-    return u + a * (environment.TIMESTEP) ** 2.0, environment.DISTRIBUTION.logpdf(f)
+    pos = u + (f * (environment.TIMESTEP) ** 2.0) / environment.MASS
+    return pos, environment.DISTRIBUTION.logpdf(f), f
 
 
 def random_walk(u, environment):
     'Langevin random walk from force for TAU / TIMESTEP steps'
-    positions = [u]
-    count = 0
-    while count != int(environment.TAU / environment.TIMESTEP):
-        u, logPr = step(positions[-1], environment)
+    positions, count = [u], int(environment.TAU / environment.TIMESTEP)
+    logPr, force = 0.0, []
+    while count != 0:
+        u, lP, f = step(positions[-1], environment)
+        # if valid then redo
         if environment.valid(positions, u):
             positions.append(u)
-            count += 1
-    return positions
+            count -= 1
+            logPr += lP
+            force.append(f)
+    return positions, logPr, force[1] - force[0]
 
 
 def monte_carlo_path_sampling(number, u, environment):
     'do number walks within environment with starting position u and velocity v'
-    plt.figure()
-    walk = [random_walk(u, environment) for _ in range(number)]
-    for w in walk:
-        plt.plot([wi[0] for wi in w], [wi[1] for wi in w])
-    plt.show()
+    walks, logPr, forces = [], [], []
+    for i in range(number):
+        w, lP, initialForce = random_walk(u, environment)
+        walks.append(w)
+        logPr.append(lP)
+        forces.append(initialForce)
+    return walks, logPr, forces
