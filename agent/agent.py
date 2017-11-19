7eeb73e4352042a46aab7060fae4e06727c2835d
David Hui <dyth2@cam.ac.uk>
2017-11-19 13:19:45 +0000

    simplify path integral

--- agent/agent.py
+++ agent/agent.py
@@ -5,6 +5,7 @@ from langevin import *
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import numpy as np
+import sys
 
 
 def entropic_forcing(logProbs, environment, forces):
@@ -14,6 +15,14 @@ def entropic_forcing(logProbs, environment, forces):
         # calculate the partial volume v and then weighted sum to path_integral
         v = np.log(sum([np.exp(lP - logProbs[i]) for lP in logProbs]))
         pathIntegral += np.multiply(f, v)
+    nom = -2.0 * environment.TC * pathIntegral * environment.TIMESTEP ** 2
+    denom = environment.TR * (len(forces) + 1) * environment.MASS
+    return nom * environment.TIMESTEP ** 2.0 / denom 
+
+
+def entropic_forcing2(logProbs, environment, forces):
+    'calculate the path integral and determine the next best move'
+    pathIntegral = sum([np.multiply(f[i], lP) for i, lP in enumerate(logProbs)])
     nom = 2.0 * environment.TC * pathIntegral * environment.TIMESTEP ** 2
     denom = environment.TR * (len(forces) + 1) * environment.MASS
     return nom / denom
@@ -46,7 +55,7 @@ debounce_entropic_forcing(pos, environment)
 """
 while True:
     walks, logProbs, f = monte_carlo_path_sampling(numSamples, pos, environment)
-    move = entropic_forcing(logProbs, environment, f)
+    move = entropic_forcing2(logProbs, environment, f)
     pos += move
     if not environment.valid(path, pos):
         print "Error: Agent in invalid environment state,", pos
@@ -54,4 +63,5 @@ while True:
     print pos
     path.append(pos)
 
-
+# TODO: new method of computing entropic forces
+# TODO: fix motion from force
