98a9c3ac16030d6d68f22377cf8a9d611fdde4e5
David Hui <dyth2@cam.ac.uk>
2018-07-25 23:57:11 +0100

    start maxwell boltzmann investigation

--- kde.py
+++ kde.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 """
-Investigate various methods of calculating KDE
+Adapted from https://stackoverflow.com/questions/18988855/fitting-maxwell-boltzman-distribution-in-python
 """
-from scipy.stats import gaussian_kde
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import numpy as np
 
+maxwell = stats.maxwell
+data = maxwell.rvs(loc=0, scale=5, size=10000)
 
-def integrate_between_points():
-    'product of integral between points'
-    
+params = maxwell.fit(data, floc=0)
+print(params)
+# (0, 4.9808603062591041)
+
+plt.hist(data, bins=20, normed=True)
+x = np.linspace(0, 25, 100)
+plt.plot(x, maxwell.pdf(x, *params), lw=3)
+plt.show()
